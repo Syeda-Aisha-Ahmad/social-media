ab18b7c82b88d9e79e64da4a996c923aab78143d
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2022-12-27 13:36:03 +0600

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App max-w-[1350px] mx-auto">
+      <Home></Home>
     </div>
   );
 }
---
+++ src/Shared/Nav/Nav.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-end">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+
+                    {/* Mobile Screen */}
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 right-0 shadow bg-blue-100 rounded-box w-52">
+                        <li><a>Home</a></li>
+                        <li><a> Media</a></li>
+                        <li><a>About</a></li>
+                        <a className="btn">Sign In</a>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div className="navbar-end hidden lg:flex">
+
+                {/* Large screen */}
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Home</a></li>
+                    <li><a> Media</a></li>
+                    <li><a>About</a></li>
+                </ul>
+                <a className="btn">Sign In</a>
+            </div>
+        </div>
+    );
+};
+
+export default Nav;
