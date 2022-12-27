10535139621f70a9497df8d7bb0406c45078008d
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2022-12-27 13:45:54 +0600

    footer created

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../../../Shared/Footer/Footer';
 import Nav from '../../../Shared/Nav/Nav';
 
 const Home = () => {
     return (
         <div>
             <Nav></Nav>
+            <Footer></Footer>
         </div>
     );
 };
